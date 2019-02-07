13284b5eb958470825df24a660ec760d559a4892
vidyavichooz <47413343+vidyavichooz@users.noreply.github.com>
2019-02-07 14:45:09 +0530

    Update power.c

--- power.c
+++ power.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
+#include <conio.h>
 int main()
 {
     int b, exp;
-
     long long result = 1;
-
     printf("Enter a base number: ");
     scanf("%d", &b);
-
     printf("Enter an exponent: ");
     scanf("%d", &exp);
-
     while (exp != 0)
     {
         result *= b;
         --exp;
     }
-
     printf("Answer = %lld", result);
-
     return 0;
+    getch();
 }
