2df5f3311b915d31cc6f37b0b73f471d0ecac277
vidyavichooz <47413343+vidyavichooz@users.noreply.github.com>
2019-02-19 12:24:29 +0530

    Create amstong.c

---
+++ amstong.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int num,v,result,digit;
+printf("enter the num value");
+scanf("%d",&num);
+v=num;
+while(v!=0);
+{
+digit=v%10;
+result+=digit*digit*digit;
+v=v/10;
+}
+if(result==num);
+printf("amstrong number");
+}
+else
+{
+printf("not amstrong");
+}
