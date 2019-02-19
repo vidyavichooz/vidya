36d9338a3e5a71d587e573184b5b31133e0f30ba
vidyavichooz <47413343+vidyavichooz@users.noreply.github.com>
2019-02-19 12:20:39 +0530

    Create btnprime.py

---
+++ btnprime.py
@@ -0,0 +1,12 @@
+def main():
+lower = int(input(" "))
+upper = int(input(""))
+for num in range(lower,upper + 1):
+if num > 1:
+for i in range(2,num):
+if (num % i) == 0:
+break
+else:
+print(num)
+if __name__ == '__main__':
+main()
