93027f527534a92b55cb96343489d2419cf0889a
vidyavichooz <47413343+vidyavichooz@users.noreply.github.com>
2019-02-21 14:18:03 +0530

    Update ap.c

--- ap.c
+++ ap.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
-#include<conio.h>
 void main()
 {
-int a,n,d,tot;
+int a,n,d,t;
 printf("enter the a,n,d");
 scanf("%d%d%d",&a,&n,&d);
-tot=a+(n-1)*d;
-printf("%d",tot);
-getch();
+t=a+(n-1)*d;
+printf("%d",t);
 }
